d436bd5eb49d422b211dcce9c54d97d1e24b3899
mohand <mohandpro50@gmail.com>
2026-09-17 19:52:20 +0300

    first commit

---
+++ LeetCode/Easy/0001-Two-Sum/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        
+    }
+};
+
+int main()
+{
+    Solution solution1;
+    solution1.romanToInt("MCMXCIV");
+}
